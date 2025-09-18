8a2df9d54e87f406dc014bc686cfa45722f35e82
Sandro Mocevic <sandro.mocev@gmail.com>
2025-09-18 01:53:27 -0400

    removed picking

--- source/util.h
+++ source/util.h
@@ -4,12 +4,11 @@
 #include <map>
 #include <random>
 #include <GLFW/glfw3.h>
-#include "meshObject.hpp"
+#include "MeshObject.h"
 
 std::string getRandomPlatonic();
-void selection_check(GLFWwindow* window, int& curr, bool& r, meshObject& base, meshObject& arm1, meshObject& arm2);
+void selection_check(GLFWwindow* window, int& curr, bool& r, MeshObject& base, MeshObject& arm1, MeshObject& arm2);
 glm::mat4 calc_view_matrix(glm::vec3 view_coords);
-int getPickedId(GLFWwindow *window);
 
 
 std::map<int, std::string> platonics =
@@ -34,29 +33,29 @@ std::string get_random_platonic()
 
 
 
-void selection_check(GLFWwindow* window, int& curr, bool& r, meshObject& base, meshObject& arm1, meshObject& arm2)
+void selection_check(GLFWwindow* window, int& curr, bool& r, MeshObject& base, MeshObject& arm1, MeshObject& arm2)
 {
-  if (glfwGetKey(window, GLFW_KEY_T))
-  {
-      curr = base.getId();
-      r = false;
-  }
+  // if (glfwGetKey(window, GLFW_KEY_T))
+  // {
+  //     curr = base.getId();
+  //     r = false;
+  // }
 
-  else if (glfwGetKey(window, GLFW_KEY_R))
-  {
-      curr = base.getId();
-      r = true;
-  }
+  // else if (glfwGetKey(window, GLFW_KEY_R))
+  // {
+  //     curr = base.getId();
+  //     r = true;
+  // }
 
-  else if (glfwGetKey(window, GLFW_KEY_1) || glfwGetKey(window, GLFW_KEY_KP_1))
-  {
-      curr = arm1.getId();
-  }
+  // else if (glfwGetKey(window, GLFW_KEY_1) || glfwGetKey(window, GLFW_KEY_KP_1))
+  // {
+  //     curr = arm1.getId();
+  // }
 
-  else if (glfwGetKey(window, GLFW_KEY_2) || glfwGetKey(window, GLFW_KEY_KP_2))
-  {
-      curr = arm2.getId();
-  }
+  // else if (glfwGetKey(window, GLFW_KEY_2) || glfwGetKey(window, GLFW_KEY_KP_2))
+  // {
+  //     curr = arm2.getId();
+  // }
 }
 
 
@@ -77,35 +76,6 @@ glm::mat4 calc_view_matrix(glm::vec3 pos)
     glm::vec3(0.0f),   // Look at the origin
     glm::vec3(0, 1, 0) // Head is looking up at the origin (set to 0,-1,0 to look upside-down)
   );
-}
-
-
-int getPickedId(GLFWwindow *window)
-{
-    glFlush();
-    // --- Wait until all the pending drawing commands are really done.
-    // Ultra-mega-over slow !
-    // There are usually a long time between glDrawElements() and
-    // all the fragments completely rasterized.
-    glFinish();
-
-    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-
-    unsigned char data[4];
-
-    double x, y;
-    glfwGetCursorPos(window, &x, &y);
-
-    std::pair<int, int> frame, win;
-    glfwGetFramebufferSize(window, &frame.first, &frame.second);
-    glfwGetWindowSize(window, &win.first, &win.second);
-
-    x = x * (frame.first / win.first);
-    y = (win.second - y) * (frame.second / win.second);
-    glReadPixels(x, y, 1, 1, GL_RGBA, GL_UNSIGNED_BYTE, data);
-
-    int pickedId = data[0];
-    return pickedId;
 }
 
 #endif
