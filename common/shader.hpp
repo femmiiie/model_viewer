572b3aee3a3010520c1e1a24c65f48ac223fa664
Sandro Mocevic <sandro.mocev@gmail.com>
2025-10-22 01:52:28 -0400

    added generic shader loader

--- common/shader.hpp
+++ common/shader.hpp
@@ -1,6 +1,13 @@
 #ifndef SHADER_HPP
 #define SHADER_HPP
 
+GLuint CompileShader(char* filepath, GLenum type);
+GLuint LoadShadersGeneric(char* vs_filepath,
+													char* tcs_filepath,
+													char* tes_filepath,
+													char* gs_filepath,
+												  char* fs_filepath);
+
 GLuint LoadShaders(const char * vertex_file_path,const char * fragment_file_path);
 
 #endif
